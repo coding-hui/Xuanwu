7542f5d3f2b35155e0b3306f253829d6bd9fe38b
coding-hui <wecoding@yeah.net>
2024-04-02 20:30:46 +0800

    fix: iam user info

--- services/xuanwu-codegen/src/main/java/top/wecoding/xuanwu/codegen/api/controller/v1/UserInfoController.java
+++ services/xuanwu-codegen/src/main/java/top/wecoding/xuanwu/codegen/api/controller/v1/UserInfoController.java
@@ -1,15 +1,13 @@
 package top.wecoding.xuanwu.codegen.api.controller.v1;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import top.wecoding.xuanwu.core.annotation.Version;
 import top.wecoding.xuanwu.core.base.R;
 import top.wecoding.xuanwu.iam.api.AuthenticationApi;
-
-import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 /**
  * @author wecoding
@@ -24,7 +22,7 @@ public class UserInfoController {
 	private final AuthenticationApi authenticationApi;
 
 	@GetMapping("/user-info")
-	public R<?> currentUserInfo(@RequestHeader(value = AUTHORIZATION, required = false) String accessToken) {
+	public R<?> currentUserInfo(@CookieValue(value = "IAM_TOKEN") String accessToken) {
 		return authenticationApi.currentUserInfo(accessToken);
 	}
 
