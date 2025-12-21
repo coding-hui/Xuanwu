f12e1541066b7be7a1d664199f0cda27b6a756a2
wecoding <wecoding@yeah.net>
2025-12-21 17:20:16 +0800

    fix: make main method public in ExamApplication build: add native build args and context directory in pom.xml

--- services/xuanwu-exam/src/main/java/top/wecoding/exam/ExamApplication.java
+++ services/xuanwu-exam/src/main/java/top/wecoding/exam/ExamApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ExamApplication {
 
-  static void main(String[] args) {
+  public static void main(String[] args) {
     SpringApplication.run(ExamApplication.class, args);
   }
 }
